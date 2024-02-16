92b323c5d5336d0397af32e8addbb3cc25501523
Limon Islam <limon4u714@gmail.com>
2024-02-17 02:39:38 +0600

    Google auth by Laravel Socialite

---
+++ app/Http/Controllers/GoogleLogin.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleLogin extends Controller
+{
+    public function redirect(){
+        return Socialite::driver('google')->redirect();
+    }
+    public function callback(){
+        $user = Socialite::driver('google')->user();
+
+        //Find User
+        $findUser = User::where('google_id', $user->id)->first();
+
+        if($findUser){
+            Auth::login($findUser);
+        }else{
+            $newUser = User::create([
+                'name' => $user->name,
+                'email' => $user->email,
+                'google_id'=> $user->id
+            ]);
+            Auth::login($newUser);
+        }
+
+        return redirect('/dashboard');
+    }
+
+
+}
